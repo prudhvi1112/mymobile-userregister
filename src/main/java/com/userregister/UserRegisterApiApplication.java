356cdc4af372e4ac4db830ac353112bc76c29d06
prudhvi <tpreddy005@gmail.com>
2024-11-21 17:23:11 +0530

    Adding Project Structure

---
+++ src/main/java/com/userregister/UserRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.userregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegisterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userregister/UserRegisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
